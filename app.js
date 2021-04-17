2b43db0519a93d89b5ea9c396f9c66f2a85b4c5d
Kiran Riaz <kiranriaz@Kirans-MBP.hsd1.ut.comcast.net>
2021-04-16 19:00:31 -0600

    added hero

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const menu = document.querySelector('#mobile-menu')
-console menuLinks = document.querySelector('.navbar-menu')
+const menuLinks = document.querySelector('.navbar-menu')
 
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
