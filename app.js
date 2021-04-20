63dd6795223a42a31a9eb4a056b2a3699221e551
Kiran Riaz <kiranriaz@Kirans-MBP.hsd1.ut.comcast.net>
2021-04-20 17:02:51 -0600

    added to js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
+const navLogo = document.querySelector('#navbar-logo')
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
@@ -7,3 +8,14 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+    //adds highlight class to menu items
+    if(window.innerWidth > 960 && scrollPos < 600)
+}
